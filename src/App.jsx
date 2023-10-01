f20a03864f688987a838616144906081abcb629b
Ripon <ripon18922@gmail.com>
2023-10-01 19:23:18 +0600

    added product

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/header/Header'
+import Shop from './components/shop/Shop'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className='app'>
      <Header></Header>
-      
-      
+     <Shop></Shop> 
     </div>
   )
 }
