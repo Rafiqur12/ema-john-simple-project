da3c103a98dd33516ca47b7110c3c279a68b1b60
Ripon <ripon18922@gmail.com>
2023-10-04 11:04:46 +0600

    Tax is added

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -11,13 +11,14 @@ const Cart = ({cart}) => {
         total = total + product.price;
         totalShipping = totalShipping + product.shipping;
     }
+    const tax = total*7/100;
     return (
         <div className='cart'>
              <h3>Order Summary</h3>
                 <p>Selected Items: {cart.length }</p>
                 <p>Total Price: ${total}</p>
                 <p>Total Shipping: ${totalShipping}</p>
-                <p>Tax: </p>
+                <p>Tax: ${tax}</p>
                 <h6>Grand Total: </h6>
         </div>
     );
