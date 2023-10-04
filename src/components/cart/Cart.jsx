629bd7746842e9d3c55ae8becaeacbc7e69e664a
Ripon <ripon18922@gmail.com>
2023-10-04 11:02:25 +0600

    Shipping is added

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -6,15 +6,17 @@ const Cart = ({cart}) => {
     
     // console.log(cart);
     let total = 0;
+    let totalShipping = 0;
     for (const product of cart){
         total = total + product.price;
+        totalShipping = totalShipping + product.shipping;
     }
     return (
         <div className='cart'>
              <h3>Order Summary</h3>
                 <p>Selected Items: {cart.length }</p>
-                <p>Total Price: {total}</p>
-                <p>Total Shipping: </p>
+                <p>Total Price: ${total}</p>
+                <p>Total Shipping: ${totalShipping}</p>
                 <p>Tax: </p>
                 <h6>Grand Total: </h6>
         </div>
